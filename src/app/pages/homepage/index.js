5c22edc6b69fb8772eb018d45e40b4dbe070e280
Vladislav Krivokon <vladeslimonade@gmail.com>
2019-03-20 19:18:25 +0200

    test header component on mobile

--- src/app/pages/homepage/index.js
+++ src/app/pages/homepage/index.js
@@ -495,5 +495,55 @@ if (document.querySelector('.homepage')) {
     //   .setTween('.slideshow__item.--first', 1, { bottom : 0 })
     //   .addIndicators({ name: '.detail__txt-description' })
     //   .addTo(controller)
+  } else {
+    $('.header').addClass('--mobile');
+
+    let scrollTimer
+    const controller = new ScrollMagic.Controller()
+    const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+    const halfViewHeight = viewHeight / 2
+
+    window.addEventListener('wheel', e => {
+      throttle(() => {
+        const delta = Math.sign(e.deltaY)
+        const currentHeight = window.scrollY || window.scrollTop || document.getElementsByTagName('html')[0].scrollTop
+        const currentPage = Math.round(currentHeight / viewHeight)
+        // const nextPage = delta > 0 ? currentPage + 1 : currentPage - 1
+        const scrollInCurrentPage = delta > 0 ? currentHeight % viewHeight : viewHeight - (currentHeight % viewHeight)
+        // console.log('view height: ', viewHeight)
+        // console.log('current height: ', currentHeight)
+        // console.log('delta', delta)
+        // console.log('currentPage', currentPage)
+        // console.log('nextPage', nextPage)
+        // console.log('scrollInCurrentPage', scrollInCurrentPage)
+        if (scrollInCurrentPage < 300) {
+          TweenLite.to(window, 1, { scrollTo: currentPage * viewHeight })
+        } else {
+          TweenLite.to(window, .5, { scrollTo: currentPage * viewHeight })
+        }
+      })
+    })
+
+    const throttle = callback => {
+      const minScrollTime = 600
+      if (!scrollTimer) {
+        scrollTimer = setTimeout(function () {
+          scrollTimer = null
+          callback()
+        }, minScrollTime)
+      }
+    }
+
+    $('.header').each(function() {
+      // page 3 Image 1
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        // offset: -halfViewHeight,
+        // duration: viewHeight + halfViewHeight
+      })
+        .setClassToggle(this, '--fixed')
+        .addIndicators()
+        .addTo(controller)
+    })
   }
 }
