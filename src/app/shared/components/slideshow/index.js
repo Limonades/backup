9d4875cc35fb8deed2b7e7ab942f57355b5db422
Vladislav Krivokon <vladeslimonade@gmail.com>
2019-03-21 15:32:34 +0200

    mobile components

--- src/app/shared/components/slideshow/index.js
+++ src/app/shared/components/slideshow/index.js
@@ -1,1 +1,13 @@
 import './index.css'
+import $ from 'jquery'
+
+$(document).ready(function() {
+  const slideshowOpenCLose = ()=> {
+    $('.slideshow__item:not(.--first)').fadeToggle();
+    $('.slideshow__expand-btn').fadeToggle();
+    $('.slideshow').toggleClass('--expanded');
+  }
+
+  $('.slideshow__expand-btn-wrap').on('click', slideshowOpenCLose);
+  $('.slideshow__close-btn-wrap').on('click', slideshowOpenCLose);
+})
