3b2cd456e56bc18e6e52e59e4edbc533184f5a5f
Vladislav Krivokon <vladeslimonade@gmail.com>
2019-04-01 20:12:07 +0300

    to check on mobile changed bg animations without scroll effects

--- src/app/pages/homepage/index.js
+++ src/app/pages/homepage/index.js
@@ -1,6 +1,6 @@
 import './index.css'
 import $ from 'jquery'
-import TweenLite from 'gsap/TweenLite'
+// import TweenLite from 'gsap/TweenLite'
 import 'gsap/ScrollToPlugin'
 import ScrollMagic from 'scrollmagic'
 import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators'
@@ -614,45 +614,45 @@ if (document.querySelector('.homepage')) {
       }
     })
 
-    let scrollTimer
+    // let scrollTimer
     const controller = new ScrollMagic.Controller()
     // const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
     const viewHeight = $('.slideshow').height() || $('.video') || Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     // const halfViewHeight = viewHeight / 2
 
-    window.addEventListener('scroll', () => {
-      throttle(() => {
-        // const delta = Math.sign(e.deltaY)
-        const currentHeight = window.scrollY || window.scrollTop || document.getElementsByTagName('html')[0].scrollTop
-        const currentPage = Math.round(currentHeight / viewHeight)
-        // const nextPage = delta > 0 ? currentPage + 1 : currentPage - 1
-        // const scrollInCurrentPage = delta > 0 ? currentHeight % viewHeight : viewHeight - (currentHeight % viewHeight)
-        // console.log(currentHeight % viewHeight)
-        // console.log('view height: ', viewHeight)
-        // console.log('current height: ', currentHeight)
-        // console.log('delta', delta)
-        // console.log('currentPage', currentPage)
-        // console.log('nextPage', nextPage)
-        // console.log('scrollInCurrentPage', scrollInCurrentPage)
-        TweenLite.to(window, .2, { scrollTo: currentPage * viewHeight })
+    // window.addEventListener('scroll', () => {
+    //   throttle(() => {
+    //     // const delta = Math.sign(e.deltaY)
+    //     const currentHeight = window.scrollY || window.scrollTop || document.getElementsByTagName('html')[0].scrollTop
+    //     const currentPage = Math.round(currentHeight / viewHeight)
+    //     // const nextPage = delta > 0 ? currentPage + 1 : currentPage - 1
+    //     // const scrollInCurrentPage = delta > 0 ? currentHeight % viewHeight : viewHeight - (currentHeight % viewHeight)
+    //     // console.log(currentHeight % viewHeight)
+    //     // console.log('view height: ', viewHeight)
+    //     // console.log('current height: ', currentHeight)
+    //     // console.log('delta', delta)
+    //     // console.log('currentPage', currentPage)
+    //     // console.log('nextPage', nextPage)
+    //     // console.log('scrollInCurrentPage', scrollInCurrentPage)
+    //     TweenLite.to(window, .2, { scrollTo: currentPage * viewHeight })
+    //
+    //     // if (scrollInCurrentPage < 50) {
+    //     //   TweenLite.to(window, .1, { scrollTo: currentPage * viewHeight })
+    //     // } else {
+    //     //   TweenLite.to(window, .1, { scrollTo: currentPage * viewHeight })
+    //     // }
+    //   })
+    // })
 
-        // if (scrollInCurrentPage < 50) {
-        //   TweenLite.to(window, .1, { scrollTo: currentPage * viewHeight })
-        // } else {
-        //   TweenLite.to(window, .1, { scrollTo: currentPage * viewHeight })
-        // }
-      })
-    })
-
-    const throttle = callback => {
-      const minScrollTime = 500
-      if (!scrollTimer) {
-        scrollTimer = setTimeout(function () {
-          scrollTimer = null
-          callback()
-        }, minScrollTime)
-      }
-    }
+    // const throttle = callback => {
+    //   const minScrollTime = 500
+    //   if (!scrollTimer) {
+    //     scrollTimer = setTimeout(function () {
+    //       scrollTimer = null
+    //       callback()
+    //     }, minScrollTime)
+    //   }
+    // }
 
     $('.header').each(function() {
       // page 3 Image 1
@@ -739,6 +739,19 @@ if (document.querySelector('.homepage')) {
         .addTo(controller)
     })
 
+    // $('.video').each(function() {
+    //   // page 3 Image 1
+    //   new ScrollMagic.Scene({
+    //     triggerElement: this,
+    //     offset: halfViewHeight,
+    //     duration: $(this).height()
+    //   })
+    //     .setPin($(this).find('.video__bg')[0], {pushFollowers: 0})
+    //     // .setClassToggle(this, '--fixed')
+    //     // .addIndicators()
+    //     .addTo(controller)
+    // })
+
     $('.video').each(function() {
       // page 3 Image 1
       new ScrollMagic.Scene({
@@ -746,7 +759,7 @@ if (document.querySelector('.homepage')) {
         offset: halfViewHeight,
         duration: $(this).height()
       })
-        .setPin(this, {pushFollowers: 0})
+        .setPin($(this).find('.video__bg')[0], {pushFollowers: 0})
         // .setClassToggle(this, '--fixed')
         // .addIndicators()
         .addTo(controller)
@@ -757,9 +770,34 @@ if (document.querySelector('.homepage')) {
       new ScrollMagic.Scene({
         triggerElement: this,
         offset: halfViewHeight,
-        duration: halfViewHeight
+        duration: $(this).height()
       })
-        .setClassToggle($(this).find('.video__copy')[0], 'fade-in')
+        .setClassToggle($(this).find('.video__bg')[0], 'fade-in')
+        // .addIndicators()
+        .addTo(controller)
+    })
+
+    $('.slideshow').each(function() {
+      // page 3 Image 1
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        offset: halfViewHeight,
+        duration: $(this).height()
+      })
+        .setPin($(this).find('.slideshow__bg')[0], {pushFollowers: 0})
+        // .setClassToggle(this, '--fixed')
+        // .addIndicators()
+        .addTo(controller)
+    })
+
+    $('.slideshow').each(function() {
+      // page 3 Image 1
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        offset: halfViewHeight,
+        duration: $(this).height()
+      })
+        .setClassToggle($(this).find('.slideshow__bg')[0], 'fade-in')
         // .addIndicators()
         .addTo(controller)
     })
