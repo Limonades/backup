e31652b27c92db681f59f921362a199bae5b7807
Ivan Melnychenko <ivmel.ca@gmail.com>
2019-04-04 20:40:03 +0300

    ALT-334 styled video player in modal

--- src/app/shared/components/video/index.js
+++ src/app/shared/components/video/index.js
@@ -3,6 +3,24 @@ import $ from 'jquery'
 import MobileDetect from 'mobile-detect/mobile-detect.min'
 
 $(document).ready(function() {
+  const mute = document.getElementById("mute");
+
+  function togglePlayPause() {
+    if (video.paused || video.ended) {
+      playpause.title = "pause";
+      video.play();
+    }
+    else {
+      playpause.title = "play";
+      video.pause();
+    }
+    $(playpause).children().toggleClass('video__btn-img--hide');
+  }
+
+  function toggleMute() {
+    video.muted = !video.muted;
+    $(mute).children().toggleClass('video__btn-img--hide');
+  }
   // no desktop
   const md = new MobileDetect(window.navigator.userAgent);
 
@@ -13,17 +31,21 @@ $(document).ready(function() {
         e.preventDefault();
         let id = $(this).attr('href');
 
+        $(`#${id} video`)[0].play();
+
         $(`#${id}`).addClass('--open');
         $('body').addClass('--no-scroll');
         $(this).parents('.video__copy').addClass('--modal-open')
-      })
+      });
 
       // close popup
       $('.video__modal').on('click', function(e) {
-        if(e.target.tagName !== 'VIDEO'){
+        if(e.target.tagName !== 'VIDEO' && !$(e.target).hasClass('video__btn') ){
           $(this).removeClass('--open');
           $('body').removeClass('--no-scroll');
           $(this).find('.video__copy').removeClass('--modal-open');
+          $(`.video__modal video`)[0].pause();
+          // $(playpause).children().toggleClass('video__btn-img--hide');
         }
       })
     } else {
@@ -33,40 +55,25 @@ $(document).ready(function() {
 
         $(`#${id}`).addClass('--open');
         $('body').addClass('--no-scroll');
-        $(this).parents('.video').addClass('--modal-open')
-      })
+        $(this).parents('.video').addClass('--modal-open');
+      });
 
       // close popup
       $('.video__modal').on('click', function(e) {
-        if(e.target.tagName !== 'VIDEO'){
+        if(e.target.tagName !== 'VIDEO' && !$(e.target).hasClass('video__btn') ){
           $(this).removeClass('--open');
           $('body').removeClass('--no-scroll');
           $(this).parents('.video').removeClass('--modal-open');
+          // $(playpause).children().toggleClass('video__btn-img--hide');
         }
       })
     }
 
     // TODO video controls
-    // var video = document.getElementById("video");
-    // var playpause = document.getElementById("playpause");
-    //
-    // function togglePlayPause() {
-    //   if (video.paused || video.ended) {
-    //     playpause.title = "pause";
-    //     playpause.innerHTML = "pause";
-    //     video.play();
-    //   }
-    //   else {
-    //     playpause.title = "play";
-    //     playpause.innerHTML = "play";
-    //     video.pause();
-    //   }
-    // }
-    //
-    // function toggleMute() {
-    //   video.muted = !video.muted;
-    // }
-    //
+    var video = document.getElementById("video");
+    var playpause = document.getElementById("playpause");
+
+
     // function updateProgress() {
     //   var progress = document.getElementById("progress");
     //   var value = 0;
@@ -75,19 +82,85 @@ $(document).ready(function() {
     //   }
     //   progress.style.width = value + "%";
     // }
-    //
-    // $('#playpause').on('click', function() {
-    //   togglePlayPause();
-    // })
-    //
-    // $('#mute').on('click', function() {
-    //   toggleMute();
-    // })
-    //
-    // video.addEventListener("timeupdate", updateProgress, false);
-    //
-    // video.addEventListener('timeupdate', function() {
-    //   $('.current').text(video.currentTime);
-    // });
+
+    $('#playpause').on('click', function() {
+      togglePlayPause();
+    });
+
+    $('#mute').on('click', function() {
+      toggleMute();
+    });
+
   }
-})
+  $(video).on('ended', function () {
+    $(playpause).children().toggleClass('video__btn-img--hide');
+  });
+
+  $(video).on('timeupdate', function() {
+
+    setTimeout(function () {
+      let minutes;
+      let seconds;
+      const time = Math.floor(video.currentTime );
+      if (+time > 59 ) {
+        minutes = parseInt( (time/60)+'');
+        seconds = (time%60) ? time % 60 : '00';
+        +seconds < 10 ? seconds = '0'+ seconds : seconds;
+        // +minutes < 10 ? minutes = '0'+ minutes : minutes;
+      } else {
+         minutes = '0';
+         seconds = time;
+         +seconds < 10 ? seconds = '0'+ seconds : seconds;
+      }
+      $('.current').html(minutes + ':' + seconds);
+    }, 1000);
+
+  });
+
+//update HTML5 video current play time
+  $(video).on('timeupdate', function() {
+    var currentPos = video.currentTime; //Get current time
+    var maxduration = video.duration; //Get video duration
+    var percentage = 100 * currentPos / maxduration + "%"; //in %
+    $('.timeBar').css({'width' : percentage });
+
+  });
+
+  var timeDrag = false;   /* Drag status */
+  $('.progressBar').mousedown(function(e) {
+    timeDrag = true;
+    updatebar(e.pageX);
+  });
+  $(document).mouseup(function(e) {
+    if(timeDrag) {
+      timeDrag = false;
+      updatebar(e.pageX);
+    }
+  });
+  $(document).mousemove(function(e) {
+    if(timeDrag) {
+      updatebar(e.pageX);
+    }
+  });
+
+//update Progress Bar control
+  var updatebar = function(x) {
+    var progress = $('.progressBar');
+    var maxduration = video.duration; //Video duraiton
+    var position = x - progress.offset().left; //Click pos
+    var percentage = 100 * position / progress.width();
+
+    //Check within range
+    if(percentage > 100) {
+      percentage = 100;
+    }
+    if(percentage < 0) {
+      percentage = 0;
+    }
+
+    //Update progress bar and video currenttime
+    $('.timeBar').css('width', percentage+'%');
+    video.currentTime = maxduration * percentage / 100;
+  };
+
+});
