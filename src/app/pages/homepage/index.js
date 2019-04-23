772330dfc7fb3565a1ddb3ea49ad75e6aa46ca5f
Vladislav Krivokon <vladeslimonade@gmail.com>
2019-04-23 12:01:06 +0300

    scroll glitch bug to test on iphone

--- src/app/pages/homepage/index.js
+++ src/app/pages/homepage/index.js
@@ -815,6 +815,7 @@ if (document.querySelector('.homepage')) {
     //   .addTo(controller)
   } else {
     $('.nav').addClass('--mobile');
+    $('body').addClass('--mobile');
     $('progress').addClass('--mobile');
     $('.header').addClass('--mobile');
     $('.detail').addClass('--mobile');
