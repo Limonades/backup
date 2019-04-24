4924c5bb98de1a6577518cceef7450e9777b255f
Vladislav Krivokon <vladeslimonade@gmail.com>
2019-04-24 17:20:29 +0300

    ALT-361 remove loader when video can play

--- src/app/shared/components/loader/index.js
+++ src/app/shared/components/loader/index.js
@@ -2,9 +2,11 @@ import './index.css'
 import $ from 'jquery'
 
 $(window).on('load', function() {
-  $('body').removeClass('--loading');
+  $('video').on('canplay', function() {
+    $('body').removeClass('--loading');
 
-  setTimeout(function() {
-    $('.page-loader-wrap').remove();
-  }, 2000)
+    setTimeout(function() {
+      $('.page-loader-wrap').remove();
+    }, 2000)
+  })
 })
