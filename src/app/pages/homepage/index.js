406475008801e2c1b8a909ad6f1d6fa7b400a607
Vladislav Krivokon <vladeslimonade@gmail.com>
2019-04-25 16:21:45 +0300

    enabled only for detail component

--- src/app/pages/homepage/index.js
+++ src/app/pages/homepage/index.js
@@ -893,9 +893,9 @@ if (document.querySelector('.homepage')) {
     }
 
     // let scrollTimer
-    // const controller = new ScrollMagic.Controller()
+    const controller = new ScrollMagic.Controller()
     // const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
-    // const viewHeight = $('.slideshow').height() || $('.video') || Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    const viewHeight = $('.slideshow').height() || $('.video') || Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     // const halfViewHeight = viewHeight / 2
 
     // window.addEventListener('scroll', () => {
@@ -1031,103 +1031,140 @@ if (document.querySelector('.homepage')) {
     //       .addTo(controller)
     //   }
     // })
-    //
-    // // $('.header').each(function() {
-    // //   // page 3 Image 1
-    // //   new ScrollMagic.Scene({
-    // //     triggerElement: this,
-    // //     offset: halfViewHeight,
-    // //     duration: $(this).height()
-    // //   })
-    // //     .setClassToggle($(this).find('.header__title-wrap')[0], 'fade-in')
-    // //     // .addIndicators()
-    // //     .addTo(controller)
-    // // })
-    //
-    // $('.detail').each(function() {
-    //   // page 3 Image 1
-    //   new ScrollMagic.Scene({
-    //     triggerElement: this,
-    //     offset: -halfViewHeight,
-    //     duration: $(this).height() + viewHeight
-    //   })
-    //     .setPin($(this).find('.detail__bg')[0], {pushFollowers: 0})
-    //     // .setClassToggle(this, '--fixed')
-    //     // .addIndicators()
-    //     .addTo(controller)
-    // })
-    //
-    // $('.detail').each(function() {
-    //   new ScrollMagic.Scene({
-    //     triggerElement: this,
-    //     offset: 0,
-    //     duration: halfViewHeight
-    //   })
-    //     .setTween($(this).find('.detail__bg')[0], 1.5, {  opacity: 1 })
-    //     // .setClassToggle($(this).find('.detail__bg')[0], 'fade-in')
-    //     // .addIndicators()
-    //     .addTo(controller)
-    // })
-    //
-    // $('.detail').each(function() {
-    //   new ScrollMagic.Scene({
-    //     triggerElement: this,
-    //     offset: $(this).height(),
-    //     duration: halfViewHeight
-    //   })
-    //     .setTween($(this).find('.detail__bg')[0], 1.5, {  opacity: 0 })
-    //     // .setClassToggle($(this).find('.detail__bg')[0], 'fade-in')
-    //     // .addIndicators()
-    //     .addTo(controller)
-    // })
-    //
-    // // to optimize fixed animation
-    // // $('.detail').each(function() {
-    // //   // page 3 Image 1
-    // //   new ScrollMagic.Scene({
-    // //     triggerElement: this,
-    // //     offset: 0,
-    // //     duration: viewHeight
-    // //   })
-    // //     .setClassToggle($(this).find('.detail__photo')[0], '--bottom')
-    // //     // .addIndicators()
-    // //     .addTo(controller)
-    // // })
-    //
-    // $('.detail').each(function() {
-    //   // page 3 Image 1
-    //   new ScrollMagic.Scene({
-    //     triggerElement: this,
-    //     offset: halfViewHeight - 10,
-    //     duration: (viewHeight + 10) * 2
-    //   })
-    //     .setPin($(this).find('.detail__photo')[0], {pushFollowers: 0})
-    //     .setClassToggle($(this).find('.detail__photo')[0], '--fixed')
-    //     // .addIndicators()
-    //     .addTo(controller)
-    // })
-    //
-    // $('.detail').each(function() {
-    //   // page 3 Image 1
-    //   new ScrollMagic.Scene({
-    //     triggerElement: this,
-    //     offset: viewHeight + halfViewHeight + 100,
-    //     duration: viewHeight
-    //   })
-    //     .setClassToggle($(this).find('.detail__photo')[0], '--top')
-    //     // .addIndicators()
-    //     .addTo(controller)
-    // })
-    //
-    // $('.detail').each(function() {
+
+    // $('.header').each(function() {
     //   // page 3 Image 1
     //   new ScrollMagic.Scene({
     //     triggerElement: this,
     //     offset: halfViewHeight,
-    //     duration: viewHeight + 200
+    //     duration: $(this).height()
     //   })
-    //     // .setPin($(this).find('.detail__photo')[0], {pushFollowers: 0})
-    //     .setClassToggle($(this).find('.detail__photo')[0], '--animated')
+    //     .setClassToggle($(this).find('.header__title-wrap')[0], 'fade-in')
+    //     // .addIndicators()
+    //     .addTo(controller)
+    // })
+
+    $('.detail').each(function() {
+      // page 3 Image 1
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        offset: -halfViewHeight,
+        duration: $(this).height() + viewHeight
+      })
+        .setPin($(this).find('.detail__bg')[0], {pushFollowers: 0})
+        // .setClassToggle(this, '--fixed')
+        // .addIndicators()
+        .addTo(controller)
+    })
+
+    $('.detail').each(function() {
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        offset: 0,
+        duration: halfViewHeight
+      })
+        .setTween($(this).find('.detail__bg')[0], 1.5, {  opacity: 1 })
+        // .setClassToggle($(this).find('.detail__bg')[0], 'fade-in')
+        // .addIndicators()
+        .addTo(controller)
+    })
+
+    $('.detail').each(function() {
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        offset: $(this).height(),
+        duration: halfViewHeight
+      })
+        .setTween($(this).find('.detail__bg')[0], 1.5, {  opacity: 0 })
+        // .setClassToggle($(this).find('.detail__bg')[0], 'fade-in')
+        // .addIndicators()
+        .addTo(controller)
+    })
+
+    // to optimize fixed animation
+    // $('.detail').each(function() {
+    //   // page 3 Image 1
+    //   new ScrollMagic.Scene({
+    //     triggerElement: this,
+    //     offset: 0,
+    //     duration: viewHeight
+    //   })
+    //     .setClassToggle($(this).find('.detail__photo')[0], '--bottom')
+    //     // .addIndicators()
+    //     .addTo(controller)
+    // })
+
+    $('.detail').each(function() {
+      // page 3 Image 1
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        offset: halfViewHeight - 10,
+        duration: (viewHeight + 10) * 2
+      })
+        .setPin($(this).find('.detail__photo')[0], {pushFollowers: 0})
+        .setClassToggle($(this).find('.detail__photo')[0], '--fixed')
+        // .addIndicators()
+        .addTo(controller)
+    })
+
+    $('.detail').each(function() {
+      // page 3 Image 1
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        offset: viewHeight + halfViewHeight + 100,
+        duration: viewHeight
+      })
+        .setClassToggle($(this).find('.detail__photo')[0], '--top')
+        // .addIndicators()
+        .addTo(controller)
+    })
+
+    $('.detail').each(function() {
+      // page 3 Image 1
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        offset: halfViewHeight,
+        duration: viewHeight + 200
+      })
+        // .setPin($(this).find('.detail__photo')[0], {pushFollowers: 0})
+        .setClassToggle($(this).find('.detail__photo')[0], '--animated')
+        // .addIndicators()
+        .addTo(controller)
+    })
+
+    $('.detail__txt-description').each(function() {
+      // page 3 Image 1
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        offset: -halfViewHeight,
+        duration: viewHeight * 2.5
+      })
+        .setPin(this, {pushFollowers: 0})
+        .setClassToggle(this, '--fixed')
+        // .addIndicators()
+        .addTo(controller)
+    })
+
+    $('.detail__txt-description').each(function() {
+      // page 3 Image 1
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        offset: halfViewHeight / 2,
+        duration: halfViewHeight / 2 + viewHeight + 80
+      })
+        .setClassToggle(this, '--animated')
+        // .addIndicators()
+        .addTo(controller)
+    })
+
+    // $('.detail__txt-description').each(function() {
+    //   // page 3 Image 1
+    //   new ScrollMagic.Scene({
+    //     triggerElement: this,
+    //     offset: halfViewHeight + viewHeight,
+    //     duration: halfViewHeight
+    //   })
+    //     .setClassToggle(this, '--top')
     //     // .addIndicators()
     //     .addTo(controller)
     // })
@@ -1136,100 +1173,63 @@ if (document.querySelector('.homepage')) {
     //   // page 3 Image 1
     //   new ScrollMagic.Scene({
     //     triggerElement: this,
-    //     offset: -halfViewHeight,
-    //     duration: viewHeight * 2.5
+    //     offset: viewHeight + halfViewHeight + 100,
+    //     duration: 90
     //   })
-    //     .setPin(this, {pushFollowers: 0})
-    //     .setClassToggle(this, '--fixed')
+    //     .setTween(this, 1, {  opacity: 0 })
     //     // .addIndicators()
     //     .addTo(controller)
     // })
-    //
-    // $('.detail__txt-description').each(function() {
+
+    $('.detail__txt-description').each(function() {
+      // page 3 Image 1
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        offset: halfViewHeight + 50,
+        duration: viewHeight + halfViewHeight
+      })
+        .setClassToggle(this, '--bg')
+        // .addIndicators()
+        .addTo(controller)
+    })
+
+    // $('.detail__img-bg').each(function() {
     //   // page 3 Image 1
     //   new ScrollMagic.Scene({
     //     triggerElement: this,
-    //     offset: halfViewHeight / 2,
-    //     duration: halfViewHeight / 2 + viewHeight + 80
+    //     offset: 0,
+    //     duration: viewHeight
     //   })
-    //     .setClassToggle(this, '--animated')
+    //     .setClassToggle(this, '--snap-off')
     //     // .addIndicators()
     //     .addTo(controller)
     // })
-    //
-    // // $('.detail__txt-description').each(function() {
-    // //   // page 3 Image 1
-    // //   new ScrollMagic.Scene({
-    // //     triggerElement: this,
-    // //     offset: halfViewHeight + viewHeight,
-    // //     duration: halfViewHeight
-    // //   })
-    // //     .setClassToggle(this, '--top')
-    // //     // .addIndicators()
-    // //     .addTo(controller)
-    // // })
-    // //
-    // // $('.detail__txt-description').each(function() {
-    // //   // page 3 Image 1
-    // //   new ScrollMagic.Scene({
-    // //     triggerElement: this,
-    // //     offset: viewHeight + halfViewHeight + 100,
-    // //     duration: 90
-    // //   })
-    // //     .setTween(this, 1, {  opacity: 0 })
-    // //     // .addIndicators()
-    // //     .addTo(controller)
-    // // })
-    //
+
     // $('.detail__txt-description').each(function() {
+    //   // to fix slide coord destination bug
+    //   new ScrollMagic.Scene({
+    //     triggerElement: this,
+    //     offset: viewHeight,
+    //     duration: halfViewHeight - 10
+    //   })
+    //     .setClassToggle(this, '--height')
+    //     // .addIndicators()
+    //     .addTo(controller)
+    // })
+
+    // $('.video').each(function() {
     //   // page 3 Image 1
     //   new ScrollMagic.Scene({
     //     triggerElement: this,
-    //     offset: halfViewHeight + 50,
-    //     duration: viewHeight + halfViewHeight
+    //     offset: halfViewHeight,
+    //     duration: $(this).height()
     //   })
-    //     .setClassToggle(this, '--bg')
+    //     .setPin($(this).find('.video__bg')[0], {pushFollowers: 0})
+    //     // .setClassToggle(this, '--fixed')
     //     // .addIndicators()
     //     .addTo(controller)
     // })
-    //
-    // // $('.detail__img-bg').each(function() {
-    // //   // page 3 Image 1
-    // //   new ScrollMagic.Scene({
-    // //     triggerElement: this,
-    // //     offset: 0,
-    // //     duration: viewHeight
-    // //   })
-    // //     .setClassToggle(this, '--snap-off')
-    // //     // .addIndicators()
-    // //     .addTo(controller)
-    // // })
-    //
-    // // $('.detail__txt-description').each(function() {
-    // //   // to fix slide coord destination bug
-    // //   new ScrollMagic.Scene({
-    // //     triggerElement: this,
-    // //     offset: viewHeight,
-    // //     duration: halfViewHeight - 10
-    // //   })
-    // //     .setClassToggle(this, '--height')
-    // //     // .addIndicators()
-    // //     .addTo(controller)
-    // // })
-    //
-    // // $('.video').each(function() {
-    // //   // page 3 Image 1
-    // //   new ScrollMagic.Scene({
-    // //     triggerElement: this,
-    // //     offset: halfViewHeight,
-    // //     duration: $(this).height()
-    // //   })
-    // //     .setPin($(this).find('.video__bg')[0], {pushFollowers: 0})
-    // //     // .setClassToggle(this, '--fixed')
-    // //     // .addIndicators()
-    // //     .addTo(controller)
-    // // })
-    //
+
     // if ($('.video').length) {
     //   $('.video').each(function() {
     //     // page 3 Image 1
